1c11d15f62b7864041da54084f031aa40a17d997
4b42 <hostmaster@4b42.com>
2018-12-30 09:53:13 +0100

    Create install.sh

---
+++ eoip/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#-----------------------------------------------------------------------#
+# Copyright 2006-2018 by Kevin Buehl <kevin.buehl@4b42.com>             #
+#-----------------------------------------------------------------------#
+#  __          __    _____________    __          __    ______________  #
+# |  |  2006  |  |  |   _______   \  |  |        |  |  |___________   | #
+# |  |  2018  |  |  |  |       \  |  |  |        |  |              |  | #
+# |  |___ ____|  |  |  |_______/  /  |  |___ ____|  |   ___________|  | #
+# |______ ____   |  |   _______  |   |______ ____   |  |   ___________| #
+#  by         |  |  |  |       \  \  Server      |  |  |  |             #
+#    Kevin    |  |  |  |_______/  |   Management |  |  |  |___________  #
+#      Buehl  |__|  |_____________/     System   |__|  |______________| #
+#                                                                       #
+# No part of this website or any of its contents may be reproduced,     #
+# copied, modified or adapted, without the prior written consent of     #
+# the author, unless otherwise indicated for stand-alone materials.     #
+# For more Information visit www.4b42.com.                              #
+# This notice must be untouched at all times.                           #
+#-----------------------------------------------------------------------#
+# https://www.4b42.com/support/kb/5c227f1f45dbe-Ethernet-over-IP-unter-Linux-Debian
